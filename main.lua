8f99aae491afcf35425ed632e148d6061ee7aeba
aren-cllc <areno2002@live.nl>
2016-07-06 12:40:21 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -874,7 +874,7 @@ end
 end
 if msgstartswith(msg, "c/") or msgstartswith(msg, "h/") or msgstartswith(msg, "l/") or msgstartswith(msg, "hl/") or msgstartswith(msg, "g/") then
 else
-firstget=true
+oldid = '['..plr.Name..']: '..msg
 game:service'HttpService':GetAsync('http://discord.crystalrepo.ml:8080/sendchats/['..plr.Name..']: '..msg)
 end
 end
