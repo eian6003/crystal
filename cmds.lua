26da18fb5931e62b4ecdfb9f21949a0c23808db1
aren-cllc <areno2002@live.nl>
2016-07-18 17:01:40 +0200

    Update cmds.lua

--- cmds.lua
+++ cmds.lua
@@ -21,35 +21,64 @@ end
 end)
 
 Crystal.AddCommand("Mailing", 0, {"mail"}, "Allows you to send an E-Mail", function(plr, msg)
-	local function GetChat()
+local as = nil
+local to = nil
+local subj = nil
+local text = nil
+local function GetAs()
 		local stop = false
 		plr.Chatted:connect(function(asd)
 			if stop == false then
 				stop = true
 				Crystal.Dismiss(plr)
-				return asd
+				as = asd
+	Crystal.Tablet(plr, "Chat the email you want to send to (example: person@gmail.com)")
+	GetTo()
 			end
 		end)
 end
-Spawn(function()
-	Crystal.Tablet(plr, "Chat the email you want to send as (example: noreply, not noreply@gmail.com)")
-	as = GetChat()
-	repeat wait() until as ~= nil;
-	Crystal.Tablet(plr, "Chat the email you want to send to (example: person@gmail.com)")
-	to = GetChat()
-	repeat wait() until to ~= nil;
+local function GetTo()
+		local stop = false
+		plr.Chatted:connect(function(asd)
+			if stop == false then
+				stop = true
+				Crystal.Dismiss(plr)
+				to = asd
 	Crystal.Tablet(plr, "Chat the subject of the E-Mail")
-	subj = GetChat()
-	repeat wait() until subj ~= nil;
+	GetSubj()
+			end
+		end)
+end
+local function GetSubj()
+		local stop = false
+		plr.Chatted:connect(function(asd)
+			if stop == false then
+				stop = true
+				Crystal.Dismiss(plr)
+				subj = asd
 	Crystal.Tablet(plr, "Chat the HTML contents of the E-Mail")
 	text = GetChat()
-	repeat wait() until text ~= nil;
+			end
+		end)
+end
+local function GetText()
+		local stop = false
+		plr.Chatted:connect(function(asd)
+			if stop == false then
+				stop = true
+				Crystal.Dismiss(plr)
+				text = asd
 	Crystal.Tablet(plr, "Sending E-Mail..")
 	result = Crystal.Mailing.sendMail(as, to, 'User', subj, text)
 	Crystal.Tablet(plr, result)
 	Crystal.Tablet(plr, "The E-Mail will arrive in about 2 seconds.")
-	end)
+			end
+		end)
+end
+Crystal.Tablet(plr, "Chat the email you want to send as (example: noreply, not noreply@gmail.com)")
+GetAs()
 end)
+
 
 Crystal.AddCommand("Killer Tablet", 2, {"killschield", "ks"}, "Spawns some killer tabs for ya", function(plr, msg)
 if tonumber(msg) then
